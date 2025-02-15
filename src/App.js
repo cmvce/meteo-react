0f142fa7a7742d8b04f00e2fca26047494284490
Coleene <acevedocoleene@gmail.com>
2025-02-15 17:53:14 +0100

    created react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='form-box'>
+        <form>
+          <input type='text' placeholder='Enter a city...' />
+          <input type='button' value='Search' />
+        </form>
+      </div>
     </div>
   );
 }
